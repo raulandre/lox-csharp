ce2c96315171eb540a810bdcddce2f9fb2a63190
Raul André <raulandre.dev@gmail.com>
2025-01-24 22:57:15 -0300

    Restructure project

---
+++ GenerateAst/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Globalization;
+
+namespace GenerateAst;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: generate_ast <output_dir>");
+            Environment.Exit(64);
+        }
+
+        var outputDir = args.First();
+
+        DefineAst(outputDir, "Expr", new List<string>() {
+            "Binary: Expr left, Token op, Expr right",
+            "Grouping: Expr expression",
+            "Literal: object value",
+            "Unary: Token op, Expr right"
+        });
+    }
+
+    private static void DefineAst(string outputDir, string baseName, List<string> types)
+    {
+        var path = $"{outputDir}/{baseName}.cs";
+        var definedTypes = types.Select(type =>
+        {
+            var typeData = type.Split(":");
+            var className = typeData.First().Trim();
+            var fields = typeData.Skip(1).First().Trim();
+
+            return DefineType(baseName, className, fields);
+        });
+
+        var fileContent = $@"
+            // Auto-generated file, do not modify directly.
+
+            namespace Interpreter;
+
+            public interface Visitor<T>
+            {{
+                {string.Join('\n',
+                    types.Select(type =>
+                    {
+                        var typeName = type.Split(":")[0].Trim();
+                        return $"public T Visit{typeName}{baseName}({typeName} {baseName.ToLower()});";
+                    }))}
+            }}
+
+            public abstract class {baseName}
+            {{
+                public abstract T Accept<T>(Visitor<T> visitor);
+            }};
+
+            {string.Join('\n', definedTypes)}
+        ";
+
+        File.WriteAllText(path, fileContent);
+    }
+
+    private static string DefineType(string baseName, string className, string fields)
+        => $@"
+            public class {className} : {baseName}
+            {{
+                {string.Join('\n',
+                    fields.Split(", ").Select(field =>
+                    $"public {field.Split(" ")[0]} {CultureInfo.CurrentCulture.TextInfo.ToTitleCase(field.Split(" ")[1])} {{ get; private set; }}")
+                    )}
+
+                public {className}({fields})
+                {{
+                    {string.Join('\n', fields.Split(", ").Select(field =>
+                        $"{CultureInfo.CurrentCulture.TextInfo.ToTitleCase(field.Split(" ")[1])} = {field.Split(" ")[1]};"))}
+                }}
+
+                public override T Accept<T>(Visitor<T> visitor)
+                {{
+                    return visitor.Visit{className}{baseName}(this);
+                }}
+            }}
+        ";
+}
