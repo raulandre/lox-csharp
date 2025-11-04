a89df2526164495ca2ef51335b600780b258c519
Raul <raulandre.dev@gmail.com>
2025-11-03 21:15:33 -0300

    Create scanner

--- Clox/src/vm.c
+++ Clox/src/vm.c
@@ -2,6 +2,7 @@
 #include "chunk.h"
 #include "debug.h"
 #include "value.h"
+#include "compiler.h"
 
 static VM vm;
 
@@ -74,9 +75,8 @@ static InterpretResult run(void) {
   #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
 
