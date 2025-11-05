48a4ad090bf1560dad016ed10d3d9f4f5df889c0
Raul <raulandre.dev@gmail.com>
2025-11-05 19:56:32 -0300

    Start parsing

--- Clox/include/common.h
+++ Clox/include/common.h
@@ -7,5 +7,6 @@
 #include <stdio.h>
 
 #define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
 
 #endif
--- Clox/include/compiler.h
+++ Clox/include/compiler.h
@@ -1,6 +1,9 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-void compile(const char *source);
+#include <stdbool.h>
+#include "chunk.h"
+
+bool compile(const char *source, Chunk *chunk);
 
 #endif // !COMPILER_H
